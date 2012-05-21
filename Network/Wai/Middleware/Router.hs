3cb211ad0d2c57b94f96ef620230248e48cc8802
Marek Dolgos <mdmarek@gmail.com>
2012-05-20 18:15:32 -0700

    Added type alias and documentation.

--- Network/Wai/Middleware/Router.hs
+++ Network/Wai/Middleware/Router.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Network.Wai.Middleware.Router (router,dir) where
+module Network.Wai.Middleware.Router (Route,router,dir) where
 
 import Data.Text (Text,splitOn)
 import Network.Wai (Request(..),Application)
+
+-- | Alias for a function which maps path pieces to applications.
+type Route = ([Text] -> Maybe Application)
 
 
 -- | Router for mapping paths to applications.
@@ -13,14 +16,14 @@ import Network.Wai (Request(..),Application)
 -- > router [ dir "/foo" fooApp
 -- >        , dir "/api" apiApp 
 -- >        ] defaultApp
-router :: [([Text] -> Maybe Application)] -> Application -> Application
+router :: [Route] -> Application -> Application
 router routes d req = case router' (pathInfo req) routes of
   Nothing -> d req
   Just  a -> a req
 
 
 -- | First matching paths' application, nothing otherwise.
-router' :: [Text] -> [([Text] -> Maybe Application)] -> Maybe Application
+router' :: [Text] -> [Route] -> Maybe Application
 router' _  []     = Nothing
 router' ps (r:rs) = case r ps of
   Nothing -> router' ps rs
@@ -28,7 +31,15 @@ router' ps (r:rs) = case r ps of
   
 
 -- | A possible web application if the path matches, nothing otherwise.
-dir :: Text -> Application -> ([Text] -> Maybe Application)
+-- 
+-- For example:
+-- 
+-- > let f = dir "/foo/api" myApp
+-- > f ["foo","api"]
+-- > True
+-- > f ["foo","spi"]
+-- > False
+dir :: Text -> Application -> Route
 dir path a = let ps = pathPieces path
              in  ( \xs -> if ps == xs 
                           then Just  a
